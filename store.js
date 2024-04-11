48d64c68809056b154c80caeaa8cec0e5f17a205
Tina Khakian <tkhakian-dehkordi@myseneca.ca>
2024-04-11 07:49:01 -0400

    remove default v array

--- store.js
+++ store.js
@@ -1,6 +1,6 @@
 // store.js
 import { atom } from 'jotai';
 
-export const favouritesAtom = atom([]);
+export const favouritesAtom = atom();
 
-export const searchHistoryAtom = atom([]);
+export const searchHistoryAtom = atom();
